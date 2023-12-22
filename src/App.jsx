cc7dfe91981ea2842c009fa68203494f07bf07b4
Mana Samiee <133782854+Manasamiee@users.noreply.github.com>
2023-12-22 13:21:08 +0100

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import '@chatscope/chat-ui-kit-styles/dist/default/styles.min.css';
 import { MainContainer, ChatContainer, MessageList, Message, MessageInput, TypingIndicator } from '@chatscope/chat-ui-kit-react';
 
 
-const API_KEY = process.env.REACT_APP_PLAYGROUND_API_KEY;
+const PLAYGROUND_API_KEY = process.env.REACT_APP_PLAYGROUND_API_KEY;
 
 
 
